36b10643ddbc627eaf9762ce4a523bb6d7bfe2b6
Ankit Gupta <ankit.gupta@rtcamp.com>
2015-09-24 18:49:33 +0530

    Added new test cases

--- tests/login.js
+++ tests/login.js
@@ -3,14 +3,13 @@ module.exports = {
 	browser
 	.url('http://ankit.rtcamp.net/wp-admin/')
 	.waitForElementVisible('body', 1000)
+	.waitForElementVisible('#user_login', 500)
 	.setValue('#user_login', 'admin')
+	.waitForElementVisible('#user_pass', 500)
 	.setValue('#user_pass' , 'rtcamp.com')
 	.click('#rememberme')
+	.waitForElementVisible('#wp-submit', 1000)
 	.click('#wp-submit')
-	.waitForElementVisible('wp-submit', 1000)
-	.click('#wp-menu-name')
-	.waitForElementVisible('wp-submit', 1000)
-	.click('.add-new-h2')
 	.end();
 	}
 };
